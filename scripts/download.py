d1f5f7421668d20d9e54abd2ef4fe5a7aa6a3b01
Esteve Soria <e.soria@lumicks.com>
2023-03-07 15:39:07 +0100

    Check if a file is already downloaded as it will be useful when the whole dataset needs to be downloaded

--- scripts/download.py
+++ scripts/download.py
@@ -1,6 +1,5 @@
-import pathlib
+from tqdm import tqdm
 import urllib.parse
-import shutil
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -26,9 +25,7 @@ class SentinelDownloader:
         self.root_url = "https://scihub.copernicus.eu/dhus/"
 
     def do_request(self, arguments: list, stream=False):
-        print(arguments)
         path_arguments = "/".join(arguments)
-        print(path_arguments)
         full_url = urllib.parse.urljoin(self.root_url, path_arguments)
 
         response = requests.get(full_url, auth=HTTPBasicAuth(os.getenv('USER'), os.getenv('PASS')), stream=stream)
@@ -40,13 +37,38 @@ class SentinelDownloader:
         response_text = self.do_request(["odata/v1", f"Products('{product_id}')/Online/$value"]).text
         return response_text == "true"
 
-    def download_product(self, product_id: str, download_path: str, try_to_download=True):
+    @staticmethod
+    def is_downloaded(file_path: str, to_be_downloaded_size: int = None):
+        if os.path.exists(file_path):
+            if to_be_downloaded_size:
+                stats = os.stat(file_path)
+                return stats.st_size == to_be_downloaded_size
+            else:
+                return True
+        return False
+
+    def download_product(self, product_id: str, download_path: str = "", try_to_download=True):
         is_online = self.is_product_online(product_id)
         if try_to_download and is_online:
-            response_raw = self.do_request(["odata/v1", f"Products('{product_id}')/$value"], stream=True).raw
-            with open(download_path, 'wb') as out_file:
-                shutil.copyfileobj(response_raw, out_file)
+            response = self.do_request(["odata/v1", f"Products('{product_id}')/$value"], stream=True)
+            if download_path == "":
+                download_path = response.headers["content-disposition"].split("=")[-1][1:-1]
+
+            file_length = int(response.headers["content-length"])
+
+            if self.is_downloaded(download_path, file_length):
+                raise Exception("File already downloaded")
+            with open(download_path, 'wb') as out_file, tqdm(
+                    desc=download_path,
+                    total=file_length,
+                    unit='iB',
+                    unit_scale=True,
+                    unit_divisor=1024,
+            ) as bar:
+                for data in response.iter_content(chunk_size=1024):
+                    size = out_file.write(data)
+                    bar.update(size)
 
 
 s = SentinelDownloader()
-s.download_product("8364b44f-aa7f-4589-8695-44b7ed7b8f65", "hahaha")
+s.download_product("8364b44f-aa7f-4589-8695-44b7ed7b8f65", "")
