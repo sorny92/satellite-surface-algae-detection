4384472877b0fc9b6cdce7a9361281d8f00d2ffb
Esteve Soria <esofabian@gmail.com>
2023-04-16 16:33:24 +0200

    Add a first version of the dataset

--- scripts/download.py
+++ scripts/download.py
@@ -1,32 +1,44 @@
-from tqdm import tqdm
-
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 import os
+import pandas as pd
+from enum import Enum
+
+class Downloader:
+    class ProductState(Enum):
+        AVAILABLE = 1
+        REQUESTED = 2
+
+    def __init__(self, user, password):
+        self.api = SentinelAPI(user, password, 'https://apihub.copernicus.eu/apihub')
+
+    def get_product_id(self, polygon, date) -> dict[str, dict]:
+        return self.api.query(polygon, date)
+
+    def request_product(self, id: str) -> ProductState:
+        self.api.download(id)
 
 
-# Para la recoleccion de datos se va a utilizar el
-# POLYGON((41.37 0.29, 41.37 0.43, 41.22 0.43, 41.22 0.29, 41.37 0.29))
+if __name__ == "__main__":
+    data_path = "dataset/DATOS_12_04_23.csv"
+    data = pd.read_csv(data_path)
+    #print(data)
+    print(data.loc[0])
+    polygon = data["POLYGON"][0]
+    date = data["Fecha"][0]
+
+    d = Downloader(os.getenv('USER'), os.getenv('PASS'))
+    d.get_product_id(polygon, date)
 
 
-## DEvolver un xml con los uuid de las imagenes de interes
-# https://scihub.copernicus.eu/dhus/search?q=footprint:"Intersects(POLYGON((41.37 0.29, 41.37 0.43, 41.22 0.43, 41.22 0.29, 41.37 0.29)))"
-
-
-## Comprobar si la uuid esta disponible en el historico
-# curl -u $USER:$PASS "https://scihub.copernicus.eu/dhus/odata/v1/Products('8364b44f-aa7f-4589-8695-44b7ed7b8f65')/Online/\$value"
-
-## Descargar o hacer trigger del retrieval del historico
-# curl -u $USER:$PASS "https://scihub.copernicus.eu/dhus/odata/v1/Products('8364b44f-aa7f-4589-8695-44b7ed7b8f65')/\$value"
-
-api = SentinelAPI(os.getenv('USER'), os.getenv('PASS'), 'https://apihub.copernicus.eu/apihub')
-api.download("8364b44f-aa7f-4589-8695-44b7ed7b8f65")
-res = api.query("POLYGON((41.37 0.29, 41.37 0.43, 41.22 0.43, 41.22 0.29, 41.37 0.29))",
-                date=("20230101", "20230120"),
-                platformname='Sentinel-2',
-                cloudcoverpercentage=(0, 30))
-for v in res:
-    print(v)
-    for k in res[v]:
-        print(f"    {k}")
-        print(f"        {res[v][k]}")
-    break
+    #
+    # api.download("8364b44f-aa7f-4589-8695-44b7ed7b8f65")
+    # res = api.query("POLYGON((41.37 0.29, 41.37 0.43, 41.22 0.43, 41.22 0.29, 41.37 0.29))",
+    #                 date=("20230101", "20230120"),
+    #                 platformname='Sentinel-2',
+    #                 cloudcoverpercentage=(0, 30))
+# for v in res:
+#     print(v)
+#     for k in res[v]:
+#         print(f"    {k}")
+#         print(f"        {res[v][k]}")
+#     break
