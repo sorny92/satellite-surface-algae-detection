4cd0ab895f20d6079226fca8d19f0c3a4f554c28
Esteve Soria <e.soria@lumicks.com>
2023-04-18 22:56:10 +0200

    Fail to use rasterio + EOreader

--- visualize.py
+++ visualize.py
@@ -1,9 +1,25 @@
+import geopandas as gpd
+import rasterio.plot
+
+from rasterio.windows import Window, from_bounds
 from eoreader.reader import Reader
 from eoreader.bands import GREEN, NDVI, CLOUDS, BLUE
 from matplotlib import pyplot as plt
 import pandas as pd
+from shapely import wkt
 from scripts.download import Downloader
 import os
+
+def load(prod, window, band):
+    """
+    Function that loads the wanted band over a proposed window and cleans the temporary directory.
+    """
+    arr = prod.load(
+        band,
+        window=window
+    )
+    prod.clean_tmp()
+    return arr[band]
 
 if __name__ == "__main__":
     data = pd.read_csv("dataset/DATOS_12_04_23.csv")
@@ -15,7 +31,24 @@ if __name__ == "__main__":
         filename = data["NOMBRE_FICHERO"][idx]
 
         prod = Reader().open(filename)
-        blue_band = prod.load([BLUE])
-        blue_band[BLUE][:, ::50, ::50].plot()
+
+        wkt_polygon = wkt.loads(polygon)
+        polygon = gpd.GeoDataFrame(index=[0], crs=prod.crs(), geometry=[wkt_polygon])
+        polygon = polygon.to_crs(prod.crs())
+        polygon.plot()
+        window_bounds = polygon.bounds.values[0]
+        print(polygon)
+        print(window_bounds)
+
+        with rasterio.open(str(prod.get_band_paths([GREEN])[GREEN])) as ds:
+            window_pix = from_bounds(*window_bounds, ds.transform)
+
+        full_image = load(prod, None, GREEN)
+        img = load(prod, polygon, GREEN)
+        print(full_image.shape)
+        bands = prod.load([GREEN])
+        full_image[0, ::10, ::10].plot()
+        bands[GREEN][:, ::10, ::10].plot()
+        # prod.plot()
         plt.show()
         break
