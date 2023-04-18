93f57b6b4f8d96f2ad82e828410e83fbc7ee1efc
Esteve Soria <e.soria@lumicks.com>
2023-04-18 21:00:50 +0200

    Be able to see all downloaded images but without rectangles

--- visualize.py
+++ visualize.py
@@ -1,10 +1,21 @@
 from eoreader.reader import Reader
-from eoreader.bands import GREEN, NDVI, CLOUDS
+from eoreader.bands import GREEN, NDVI, CLOUDS, BLUE
 from matplotlib import pyplot as plt
-
+import pandas as pd
+from scripts.download import Downloader
+import os
 
 if __name__ == "__main__":
-    file = "S2A_MSIL2A_20230325T112111_N0509_R037_T29SPD_20230325T172652.zip"
-    prod = Reader().open(file)
-    prod.plot()
-    plt.show()
+    data = pd.read_csv("dataset/DATOS_12_04_23.csv")
+    d = Downloader(os.getenv('USER'), os.getenv('PASS'))
+    for idx in range(data.shape[0]):
+        print(data.loc[idx])
+        polygon = data["POLYGON"][idx]
+        date = data["Fecha"][idx]
+        filename = data["NOMBRE_FICHERO"][idx]
+
+        prod = Reader().open(filename)
+        blue_band = prod.load([BLUE])
+        blue_band[BLUE][:, ::50, ::50].plot()
+        plt.show()
+        break
