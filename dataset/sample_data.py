cd9f7a9b9875631ab07c2257555fba8eff073658
Esteve Soria <esofabian@gmail.com>
2023-06-28 20:43:02 +0200

    Process a product in a class for better reuse of compute power

--- dataset/sample_data.py
+++ dataset/sample_data.py
@@ -1,4 +1,4 @@
-from processing import generate_stack, get_bbox_with_window
+from processing import ProcessProduct
 import pandas as pd
 from eoreader.reader import Reader
 from shapely import wkt
@@ -19,20 +19,24 @@ def get_roi_from_polygon(polygon_string: str, product=None):
 
 def visualize_stack(image_stack):
     import cv2
+    import numpy as np
     plt.figure(figsize=(10, 10))
     chop = da.concatenate([image_stack[3:4], image_stack[2:3], image_stack[1:2]])
     chop = da.moveaxis(chop, 0, -1)
+    chop *= 255
+    numpy_data = chop.compute()
+    numpy_data = numpy_data.astype(np.uint8)
 
-    lab= cv2.cvtColor(chop.compute(), cv2.COLOR_RGB2LAB)
+    lab = cv2.cvtColor(numpy_data, cv2.COLOR_RGB2LAB)
     l_channel, a, b = cv2.split(lab)
 
     # Applying CLAHE to L-channel
     # feel free to try different values for the limit and grid size:
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     cl = clahe.apply(l_channel)
 
     # merge the CLAHE enhanced L-channel with the a and b channel
-    limg = cv2.merge((cl,a,b))
+    limg = cv2.merge((cl, a, b))
 
     # Converting image from LAB Color model to BGR color spcae
     enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2RGB)
@@ -48,15 +52,14 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     data = pd.read_csv("dataset/raw_data/DATOS_12_04_23.csv")
 
-    for idx in range(data.shape[0]):
-        print(data.loc[idx])
-        polygon_str = data["POLYGON"][idx]
-        date = data["Fecha"][idx]
-        filename = data["NOMBRE_FICHERO"][idx]
+    for filename in data["NOMBRE_FICHERO"].unique():
         prod = Reader().open(filename)
+        pp = ProcessProduct(product=prod)
+        for idx, row in data[data["NOMBRE_FICHERO"] == filename].iterrows():
+            polygon_str = row["POLYGON"]
+            date = row["Fecha"]
 
-        polygon = get_roi_from_polygon(polygon_str, prod)
-        window = get_bbox_with_window(prod, polygon, 64)
-        stack = generate_stack(prod, prod.get_existing_bands(), window)
-        visualize_stack(stack)
-        break
+            polygon = get_roi_from_polygon(polygon_str, prod)
+            window = pp.get_bbox_from_window(polygon, 64)
+            stack = pp.generate_stack(prod.get_existing_bands(), window)
+            visualize_stack(stack)
--- processing.py
+++ processing.py
@@ -1,5 +1,5 @@
+import eoreader.products
 from eoreader.bands import SpectralBandNames
-from eoreader.products import product
 import rasterio
 import numpy as np
 from utils.bands import band_resolutions
@@ -7,86 +7,92 @@ import xarray
 from logging import debug, warning, info
 
 
-def get_bbox_with_window(prod: product, polygon, window_size: int):
-    """
-    Retrieves a bounding box with a specified window size around a given polygon within a product.
-
-    Parameters:
-        - product (rasterio.DatasetReader): The rasterio dataset representing the product.
-        - polygon (shapely.geometry.Polygon): The polygon for which to calculate the bounding box.
-        - window_size: The size of the window around the polygon in the product's coordinate system.
-
-    Returns:
-        numpy.ndarray: A numpy array representing the bounding box coordinates [x_min, y_max, x_max, y_min].
-
-    Raises:
-        None.
-
-    Example:
-        # Import the required libraries
-        import rasterio
-        import numpy as np
-        from shapely.geometry import Polygon
-
-        # Open the product using rasterio
-        product = rasterio.open('product.tif')
-
-        # Create a polygon representing the area of interest
-        polygon = Polygon([(10, 20), (20, 20), (20, 30), (10, 30)])
-
-        # Define the window size
-        window_size = 100
-
-        # Call the function to get the bounding box with the window
-        bbox = get_bbox_with_window(product, polygon, window_size)
-
-        # Print the resulting bounding box
-        print(bbox)
-    """
-    info("Extract squared bbox from a product")
+class ProcessProduct:
     sentinel2_tile_size = (10980, 10980)
-    bbox_prod = prod.footprint().bounds.values[0]
-    # print(bbox_prod)
-    left, bottom, right, top = polygon.bounds.values[0]
-    # print(left, bottom, right, top)
-    at = rasterio.transform.from_bounds(*bbox_prod, sentinel2_tile_size[0], sentinel2_tile_size[1])
-    # print(at)
-    r, c = rasterio.transform.rowcol(at, [left, right], [bottom, top])
-    # rows_center = np.mean(r)
-    rows_center = r[0]
-    # print(rows_center)
-    # columns_center = np.mean(c)
-    columns_center = c[0]
-    # print(columns_center)
 
-    x, y = rasterio.transform.xy(at, [rows_center - window_size, rows_center + window_size],
-                                 [columns_center - window_size, columns_center + window_size])
-    # print(x, y)
-    info(f"The bbox is: {np.array([x[0], y[1], x[1], y[0]])}")
-    return np.array([x[0], y[1], x[1], y[0]])
+    def __init__(self, product: eoreader.products.Product):
+        self._prod = product
+        self._product_footprint = None
+        self._affine_transform = None
 
+    def get_affine_transform(self):
+        if not self._affine_transform:
+            if not self._product_footprint:
+                self._product_footprint = self._prod.footprint()
+            bbox_prod = self._product_footprint.bounds.values[0]
+            self._affine_transform = rasterio.transform.from_bounds(*bbox_prod,
+                                                                    self.sentinel2_tile_size[0],
+                                                                    self.sentinel2_tile_size[1])
+        return self._affine_transform
 
-def generate_stack(prod, bands, window, resample=True):
-    bands_data = []
-    biggest_shape = None
-    for band in bands:
-        arr = prod.stack(
-            band,
-            window=window,
-            resolution=band_resolutions[band]
-        )
-        if band is SpectralBandNames.BLUE:
-            biggest_shape = arr.shape
-            info(biggest_shape)
-        bands_data.append(arr)
-    if resample:
-        for idx, band_data in enumerate(bands_data):
-            resampled_band = band_data.rio.reproject(
-                band_data.rio.crs,
-                shape=biggest_shape[1:],
-                resampling=rasterio.enums.Resampling.bilinear,
+    def get_bbox_from_window(self, polygon, window_size: int):
+        """
+        Retrieves a bounding box with a specified window size around a given polygon within a product.
+
+        Parameters:
+            - polygon (shapely.geometry.Polygon): The polygon for which to calculate the bounding box.
+            - window_size: The size of the window around the polygon in the product's coordinate system.
+
+        Returns:
+            numpy.ndarray: A numpy array representing the bounding box coordinates [x_min, y_max, x_max, y_min].
+
+        Raises:
+            None.
+
+        Example:
+            # Import the required libraries
+            import rasterio
+            import numpy as np
+            from shapely.geometry import Polygon
+
+            # Open the product using rasterio
+            product = rasterio.open('product.tif')
+
+            # Create a polygon representing the area of interest
+            polygon = Polygon([(10, 20), (20, 20), (20, 30), (10, 30)])
+
+            # Define the window size
+            window_size = 100
+
+            # Call the function to get the bounding box with the window
+            bbox = get_bbox_with_window(product, polygon, window_size)
+
+            # Print the resulting bounding box
+            print(bbox)
+        """
+        info("Extract squared bbox from a product")
+        left, bottom, right, top = polygon.bounds.values[0]
+        at = self.get_affine_transform()
+        r, c = rasterio.transform.rowcol(at, [left, right], [bottom, top])
+        rows_center = r[0]
+        columns_center = c[0]
+
+        x, y = rasterio.transform.xy(at, [rows_center - window_size, rows_center + window_size],
+                                     [columns_center - window_size, columns_center + window_size])
+        info(f"The bbox is: {np.array([x[0], y[1], x[1], y[0]])}")
+        return np.array([x[0], y[1], x[1], y[0]])
+
+    def generate_stack(self, bands, window, resample=True):
+        bands_data = []
+        biggest_shape = None
+        for band in bands:
+            arr = self._prod.stack(
+                band,
+                window=window,
+                resolution=band_resolutions[band]
             )
-            bands_data[idx] = resampled_band
-        bands_data = xarray.concat(bands_data, dim="z", join="override")
-        debug(bands_data.shape)
-    return bands_data
+            if band is SpectralBandNames.BLUE:
+                biggest_shape = arr.shape
+                info(biggest_shape)
+            bands_data.append(arr)
+        if resample:
+            for idx, band_data in enumerate(bands_data):
+                resampled_band = band_data.rio.reproject(
+                    band_data.rio.crs,
+                    shape=biggest_shape[1:],
+                    resampling=rasterio.enums.Resampling.bilinear,
+                )
+                bands_data[idx] = resampled_band
+            bands_data = xarray.concat(bands_data, dim="z", join="override")
+            debug(bands_data.shape)
+        return bands_data
