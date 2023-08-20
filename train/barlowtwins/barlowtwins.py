a05fcd73bb73dad6f475b8654f6d946910566525
Esteve Soria <esofabian@gmail.com>
2023-08-20 19:27:38 +0200

    Add Barlow Twins training code

---
+++ train/barlowtwins/barlowtwins.py
@@ -0,0 +1,52 @@
+from torch import nn
+import torch
+from models.resnet import ResNet, Bottleneck
+
+
+def _resnet50():
+    return ResNet(Bottleneck, [3, 4, 6, 3], 12, zero_init_residual=True)
+
+
+def off_diagonal(x):
+    # return a flattened view of the off-diagonal elements of a square matrix
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+
+class BarlowTwins(nn.Module):
+    def __init__(self, batch_size, lambd, projector, ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.lambd = lambd
+        self.backbone = _resnet50()
+        self.backbone.fc = nn.Identity()
+
+        # projector
+        sizes = [2048] + list(map(int, projector.split('-')))
+        layers = []
+        for i in range(len(sizes) - 2):
+            layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=False))
+            layers.append(nn.BatchNorm1d(sizes[i + 1]))
+            layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.Linear(sizes[-2], sizes[-1], bias=False))
+        self.projector = nn.Sequential(*layers)
+
+        # normalization layer for the representations z1 and z2
+        self.bn = nn.BatchNorm1d(sizes[-1], affine=False)
+
+    def forward(self, y1, y2):
+        z1 = self.projector(self.backbone(y1))
+        z2 = self.projector(self.backbone(y2))
+
+        # empirical cross-correlation matrix
+        c = self.bn(z1).T @ self.bn(z2)
+
+        # sum the cross-correlation matrix between all gpus
+        c.div_(self.batch_size)
+        torch.distributed.all_reduce(c)
+
+        on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
+        off_diag = off_diagonal(c).pow_(2).sum()
+        loss = on_diag + self.lambd * off_diag
+        return loss
--- train/model.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-
-class AutoEncoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        se
