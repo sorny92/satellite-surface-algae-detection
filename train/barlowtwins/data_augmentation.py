37b2148b6827fde78363c11a4994d1d830b87518
Esteve Soria <esofabian@gmail.com>
2023-08-26 17:04:30 +0200

    Add mean and std values for normalization

--- train/barlowtwins/data_augmentation.py
+++ train/barlowtwins/data_augmentation.py
@@ -4,6 +4,10 @@ import torchvision.transforms as transforms
 from .transforms.custom import BandsJitter, GaussianBlur
 
 
+def totensor(img):
+    return img.type(torch.Tensor)
+
+
 class Transform:
     def __init__(self):
         self.transform = transforms.Compose([
@@ -20,11 +24,19 @@ class Transform:
             #     p=0.8
             # ),
             # transforms.RandomGrayscale(p=0.2),
-            #GaussianBlur(p=1.0),
+            # GaussianBlur(p=1.0),
             # Solarization(p=0.0),
-            transforms.Lambda(lambda x: x.type(torch.Tensor)/32768)
-            #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            transforms.Lambda(totensor),
+            # transforms.Normalize(mean=[0.485, 0.456, 0.406],
             #                     std=[0.229, 0.224, 0.225])
+            transforms.Normalize(
+                mean=[1353.47241558, 1116.58903239, 1040.76400427, 944.92769168, 1197.13633491,
+                      2001.1258392, 2372.19987777, 2299.35905499, 731.49383711, 12.09341896,
+                      1819.96556774, 1117.73135099, 2597.75184181],
+                std=[1795.36927147, 3369.6875368, 3728.19663279, 5280.00906632,
+                     4135.21948761, 7225.77528867, 9559.73767298, 10511.2101723,
+                     2448.69314796, 19.59578514, 7313.52696182, 5577.66424732,
+                     10917.50918372])
         ])
         self.transform_prime = transforms.Compose([
             transforms.RandomHorizontalFlip(p=0.5),
@@ -35,7 +47,6 @@ class Transform:
                 p=0.8
             ),
 
-
             # transforms.RandomApply(
             #     [transforms.ColorJitter(brightness=0.4, contrast=0.4,
             #                             saturation=0.2, hue=0.1)],
@@ -44,8 +55,15 @@ class Transform:
             # transforms.RandomGrayscale(p=0.2),
             # GaussianBlur(p=0.1),
             # Solarization(p=0.2),
-            # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-            #                     std=[0.229, 0.224, 0.225])
+            transforms.Lambda(totensor),
+            transforms.Normalize(
+                mean=[1353.47241558, 1116.58903239, 1040.76400427, 944.92769168, 1197.13633491,
+                      2001.1258392, 2372.19987777, 2299.35905499, 731.49383711, 12.09341896,
+                      1819.96556774, 1117.73135099, 2597.75184181],
+                std=[1795.36927147, 3369.6875368, 3728.19663279, 5280.00906632,
+                     4135.21948761, 7225.77528867, 9559.73767298, 10511.2101723,
+                     2448.69314796, 19.59578514, 7313.52696182, 5577.66424732,
+                     10917.50918372])
         ])
 
     def __call__(self, x):
