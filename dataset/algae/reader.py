f1f2053ac86ece0f8de5027268b7c36eacd6e043
Esteve Soria <esofabian@gmail.com>
2023-08-30 00:35:49 +0200

    Add reader for algae dataset

---
+++ dataset/algae/reader.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+import pathlib
+import numpy as np
+
+
+class Algae(Dataset):
+    def __init__(self, dataset_path, transforms=None):
+        self.dataset_path = pathlib.Path(dataset_path)
+        self.image_paths = []
+        self.transforms = transforms
+        self.dataset_root = pathlib.Path(dataset_path).parent
+        self.image_paths.extend((self.dataset_path / "algae").glob("*"))
+        self.image_paths.extend((self.dataset_path / "no_algae").glob("*"))
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        p = pathlib.Path(self.image_paths[idx])
+        label, path = p.parent.name, p
+        im = np.load(self.dataset_root / path)
+        im = np.concatenate((im[:9, ...], np.zeros((1, im.shape[-1], im.shape[-1])), im[9:, ...]))
+        im = torch.from_numpy(im)
+        if self.transforms:
+            im = self.transforms(im)
+        return im, label
+
+
+if __name__ == "__main__":
+    import sys
+
+    es = Algae(sys.argv[1])
+    item = es[0]
+    print(item)
---
+++ inference/run.py
@@ -0,0 +1,31 @@
+import torch
+from train.barlowtwins import barlowtwins
+
+if __name__ == "__main__":
+    torch.cuda.set_device(1)
+    # torch.backends.cudnn.benchmark = True
+
+    model = barlowtwins.BarlowTwins(1, 1, "1-1-1").backbone
+    # print(model)
+    # weights = torch.load("checkpoint/resnet50.pth", "cpu")
+    weights = torch.load(
+        "/home/esteve/fast_folder/satellite-surface-algae-detection/runs/Aug26_01-30-06_pop-os/iter_365.pth",
+        "cpu")
+    new_state_dict = {}
+    for key, value in weights["model"].items():
+        #print(key, )
+        new_state_dict[".".join(key.split(".")[2:])] = value
+
+    missing_keys, unexpected_keys = model.load_state_dict(new_state_dict, strict=False)
+    #model.fc.weight.data.normal_(mean=0.0, std=0.01)
+    #model.fc.bias.data.zero_()
+    model.eval()
+    model.half()
+
+    import dataset.algae.reader as reader
+
+    r = reader.Algae("/home/esteve/fast_folder/satellite-surface-algae-detection/dataset/algae")
+    for v in r:
+        input = v[0].type(torch.Tensor).unsqueeze(0).half()
+        prediction = model(input)
+        print(prediction[0][512])
