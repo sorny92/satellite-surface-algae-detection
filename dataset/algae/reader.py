5bf4b7a980e34db2616d7669b1255b675ad9a289
Esteve Soria <esofabian@gmail.com>
2023-09-02 12:27:52 +0200

    Add finetune code for algae detection

--- dataset/algae/reader.py
+++ dataset/algae/reader.py
@@ -9,11 +9,17 @@ class Algae(Dataset):
         self.dataset_path = pathlib.Path(dataset_path)
         self.image_paths = []
         self.transforms = transforms
-        self.dataset_root = pathlib.Path(dataset_path).parent
-        self.image_paths.extend((self.dataset_path / "algae").glob("*"))
-        self.image_paths.extend((self.dataset_path / "no_algae").glob("*"))
-        if with_incognito:
-            self.image_paths.extend((self.dataset_path / "incognito").glob("*"))
+        if self.dataset_path.is_dir():
+            self.dataset_root = pathlib.Path(dataset_path).parent
+            self.image_paths.extend((self.dataset_path / "algae").glob("*"))
+            self.image_paths.extend((self.dataset_path / "no_algae").glob("*"))
+            if with_incognito:
+                self.image_paths.extend((self.dataset_path / "incognito").glob("*"))
+        else:
+            with open(dataset_path, "r") as f:
+                for r in f.readlines():
+                    self.image_paths.append(r.strip())
+
 
     def __len__(self):
         return len(self.image_paths)
@@ -21,14 +27,29 @@ class Algae(Dataset):
     def __getitem__(self, idx):
         p = pathlib.Path(self.image_paths[idx])
         label, path = p.parent.name, p
-        im = np.load(self.dataset_root / path)
+        if self.dataset_path.is_dir():
+            img_path = self.dataset_root / path
+        else:
+            img_path = path
+        im = np.load(img_path)
         ## Add the missing channel from eoreader to get 13
         if im.shape[0] == 12:
             im = np.concatenate((im[:9, ...], np.zeros((1, im.shape[-1], im.shape[-1])), im[9:, ...]))
         im = torch.from_numpy(im)
         if self.transforms:
             im = self.transforms(im)
-        return im, label
+        if self.dataset_path.is_dir():
+            if label == "incognito":
+                label = "no_algae"
+            return im, label
+        else:
+            if label == "incognito":
+                label = [0.1]
+            if label == "no_algae":
+                label = [0.0]
+            else:
+                label = [1.0]
+            return im, torch.FloatTensor(label)
 
 
 if __name__ == "__main__":
